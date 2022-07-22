d6a8d3f09137661ad473eec9625e8cb0f1ed42ea
Alex-Victor <alexvictoribeiro@gmail.com>
2022-07-22 00:28:58 -0300

    corrigi erro em ClienteRepository

--- src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
@@ -2,6 +2,6 @@ package br.com.petz.clientepet.cliente.application.repository;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
 
-public interface clienteRepository {
-Cliente salva(Cliente cliente);
+public interface ClienteRepository {
+	Cliente salva(Cliente cliente);
 }
